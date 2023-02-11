8b10fea534583fefb9456fc58f70593c4b66806e
Shalltear Bloodfallen <shalltearbloodfallen@vivaldi.net>
2023-02-11 19:13:08 +0000

    complete

--- week_3_data_warehouse/homework/query.sql
+++ week_3_data_warehouse/homework/query.sql
@@ -16,4 +16,37 @@ OPTIONS (
 select count(*) as counter
 from datazoomcamp.fhv_2019;
 
+-- question 2
+select distinct count(affiliated_base_number) as counter
+from `datazoomcamp.fhv_2019_big_query`;
+
+select distinct count(affiliated_base_number) as counter
+from `datazoomcamp.fhv_2019`;
+
+-- question 3
+select count(*) as counter
+from `datazoomcamp.fhv_2019_big_query`
+where PUlocationID is null
+and DOlocationID is null
+
+-- question 4
+
+create table if not exists
+datazoomcamp.fhv_2019_partion_cluster
+partition by date(pickup_datetime)
+cluster by affiliated_base_number as
+(select *
+from `datazoomcamp.fhv_2019_big_query`);
+
+-- question 5
+
+select distinct Affiliated_base_number 
+from `datazoomcamp.fhv_2019_big_query`
+where pickup_datetime >= '2019-03-01'
+and pickup_datetime <= '2019-03-31';
+
+select distinct Affiliated_base_number 
+from `datazoomcamp.fhv_2019_partion_cluster`
+where pickup_datetime >= '2019-03-01'
+and pickup_datetime <= '2019-03-31';
 
